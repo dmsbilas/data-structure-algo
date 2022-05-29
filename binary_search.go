c3ae6af8906e3d6916b5ecfad596bc1e9338caff
Bilas Siddiq <abuhaidersiddiq@Abus-MacBook-Pro.local>
2022-05-29 11:34:17 +0600

    added binary search

---
+++ binary_search.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func binary_search(heystack []int, niddle int) {
+
+}
+
+func main() {
+	dataArray := []int{2, 4}
+	binary_search(dataArray, 4)
+	fmt.Println("Working the main function using GO")
+}
