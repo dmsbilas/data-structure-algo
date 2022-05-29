558e92b583715e2c26642b7c1120134c0a3706fb
Bilas Siddiq <abuhaidersiddiq@Abus-MacBook-Pro.local>
2022-05-29 17:56:14 +0600

    binary search added

--- binary_search.go
+++ binary_search.go
@@ -1,13 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func binary_search(heystack []int, niddle int) {
+func binary_search(haystack []int, niddle int) int {
 
+	left := 0
+	right := len(haystack) - 1
+
+	var result int
+
+	for left <= right {
+		mid := int(math.Ceil(float64(left+right) / 2))
+
+		if haystack[mid] == niddle {
+			result = mid
+		}
+		if haystack[mid] < niddle {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return result
 }
 
 func main() {
-	dataArray := []int{2, 4}
-	binary_search(dataArray, 4)
-	fmt.Println("Working the main function using GO")
+	dataArray := []int{2, 4, 6, 8, 10, 12, 14, 16, 18}
+	fmt.Println(binary_search(dataArray, 12))
+
 }
